8433050c16f9d40b307b1f549bae6c4f29bb29dc
francisco jose barreiro herran <franciscojbh1993@gmail.com>
2015-12-28 02:25:35 +0800

    clean code

--- TicTacToe/TicTacToe_Library/BoardInterface.cpp
+++ TicTacToe/TicTacToe_Library/BoardInterface.cpp
@@ -7,10 +7,11 @@ void BoardInterface::showBoard(){
 		for (int j = 0; j < BOARD_COLUMN; j++) {
 			position = new Position2D(i, j);
 			value = getValue(*position);
+			delete position;
+
 			if (value == VOID) cout << " " << "|";
 			if (value == CROSS) cout << "X" << "|";
-			if (value == CIRCLE) cout << "O" << "|";
-			delete position;
+			if (value == CIRCLE) cout << "O" << "|";			
 		}
 		cout << endl << "------" << endl;
 	}
